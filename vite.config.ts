bebaf94a328e9849b3e753d018f5d1818a8e7628
Borison <84581646+borisonwong@users.noreply.github.com>
2023-07-24 23:47:39 -0600

    Updates

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://github.com/borisonwong/hkutimetable2023/",
   plugins: [react()],
 })
